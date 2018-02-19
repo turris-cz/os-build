7c521771d289270af9e208fefb5407591688321c
Michal Hrusecky <Michal@Hrusecky.net>
2018-02-19 16:34:08 +0100

    Fix package generation regexp
    Signed-off-by: Michal Hrusecky <Michal@Hrusecky.net>

--- helpers/generate_userlists.sh
+++ helpers/generate_userlists.sh
@@ -71,8 +71,8 @@ mkdir -p $OUTPUT_PATH
 M4ARGS="--include=lists -D _INCLUDE_=lists/ -D _BRANCH_=$BRANCH -D _BOARD_=$BOARD"
 $MINIMAL && M4ARGS="$M4ARGS -D _BRANCH_FALLBACK_=nightly"
 
-sed -i 's|subdirs = {"base"[}]*}|subdirs = {"core"'"$(
-	cat "$SRC_DIR"/feeds.conf | sed 's|#.*||' | sed 's|src-git \([^[:blank:]]*\) .*|, "\1"|' | tr '\n' ' '
+sed -i 's|subdirs = {"base"[^}]*}|subdirs = {"core"'"$(
+	cat "$SRC_DIR"/feeds.conf | sed 's|#.*||' | grep '.' | sed 's|src-git \([^[:blank:]]*\) .*|, "\1"|' | tr '\n' ' '
 )}|" "$SRD_DIR/lists/repository.m4"
 
 cd "$SRC_DIR"
