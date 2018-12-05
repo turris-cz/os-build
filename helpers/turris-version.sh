c7085e070ba7c644bcec091988e7ab7d5f545de2
Karel Kočí <karel.koci@nic.cz>
2018-12-05 14:35:21 +0100

    turris-version: improve version parsing

--- helpers/turris-version.sh
+++ helpers/turris-version.sh
@@ -18,29 +18,18 @@
 news_file="$(dirname "$(dirname "$(readlink -f "$0")")")/NEWS"
 
 get_version() {
-	# If first grep fails - version is not first non-empty, add 999 to the end
-	# Be aware that Condition in if also outputs the results
-	if ! grep . "$news_file" | head -n 1 | grep '^[0-9.]\+$'; then
-		echo "$(grep '^[0-9.]\+$' "$news_dir" | head -n 1).999"
-	fi
+	sed -n '/^-\+$/{x;p;q}; x' "$news_file"
+}
+
+news_text() {
+	awk '/^-+$/{if(flg) nextfile; else flg=1;next} flg==1{prev=$0;flg=2;next} flg==2{print prev;prev=$0}' "$news_file" | \
+		sed '/^$/d;s/^[[:blank:]]*\*[[:blank:]]*/ • /'
 }
 
 gen_package() {
 	version="$(get_version)"
 	mkdir -p "$output"
 	cd "$output"
-	# If top of the NEWS is not a version number, find previous one and add 99
-	# Also try to figure out where are the actual NEWS for this version
-	if expr "$version" : '999$' > /dev/null; then
-		start=1
-		end="$(grep -n '^[0-9.]\+$' "$news_file" | sed -n '1 s|:.*||p')"
-		end=$((end - 1))
-	else
-		start="$(grep -n '^[0-9.]\+$' "$news_file" | sed -n '1 s|:.*||p')"
-		start=$((start + 1))
-		end="$(grep -n '^[0-9.]\+$' "$news_file" | sed -n '2 s|:.*||p')"
-		[ -n "$end" ] || end="\$"
-	fi
 	# Generate package
 	cat > Makefile << EOF
 #
@@ -73,7 +62,7 @@ define Package/turris-version/postinst
 #!/bin/sh
 # Danger: spaces are not ordinary spaces, but special unicode ones
 [ -n "\$\$IPKG_INSTROOT" ] || {
-create_notification -s news "$(sed -n "$start,$end p" "$news_file" | sed '/^$/d;s/^[[:blank:]]*\*[[:blank:]]*/ • /')"
+create_notification -s news "$(news_text | sed 's/"/\\"/')"
 }
 endef
 
