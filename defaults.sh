cf6737efb6af4ecb69a6c91e9a469849fd9a4ea6
Karel Kočí <karel.koci@nic.cz>
2019-02-18 16:11:24 +0100

    Document release and steps for new version branch

---
+++ defaults.sh
@@ -0,0 +1,8 @@
+# This file contains project wide defaults. They are here because we have to tweak
+# these defaults every time we fork new release.
+
+# This sets default publish branch
+# Expected values are:
+#   "hbd" for master branch
+#   "hbs" for release branch
+PUBLISH_BRANCH="hbs"
