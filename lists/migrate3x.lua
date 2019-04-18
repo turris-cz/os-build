47d94a01a86abdda850eed9a50f837cafa65baf3
Karel Kočí <karel.koci@nic.cz>
2019-04-18 10:26:29 +0200

    lists: migrate3x: increase priority of turris repository

--- lists/migrate3x.lua
+++ lists/migrate3x.lua
@@ -22,6 +22,7 @@ if not version_match or not self_version or version_match(self_version, "<63.0")
 	]]
 	-- TODO move it to hbs when we have v63.0 in hbs
 	Repository("turris", "https://repo.turris.cz/hbk/packages/" .. board, {
+		priority = 60,
 		subdirs = { "base", "core", "packages", "turrispackages"}
 	})
 
