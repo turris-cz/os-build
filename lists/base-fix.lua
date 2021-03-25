turris-cz/os-build

commit b50ca55cdcf4bcd6482ebd7b0eedb4597c92bc3a

lists/base-fix: Reset u-boot environment configuration
This triggers removal of uboot environment configuration from system so
we can set there correct one by installing new version of uboot-tools.

In case of Turris Mox we have to do also additional trickery as file we
want to generate is part of mox-support package originally. With this
update it is dropped but we have to let it be removed fist before we
recreate it by installing new version of uboot-tools otherwise updater
would remove that file as part of mox-support package cleanup phase.

lists/base-fix.lua
@@ -136,3 +136,16 @@ if installed and installed["luci-lighttpd"] and not installed["turris-webapps-lu
 	-- interfere with install priorities.
 	Install("luci-base", { reinstall = true, condition = "luci-base" })
 end
+
+-- With uboot-tools version 2018.03-4 environment configuration was fixed. Problem
+-- is that it is not applied in default as script checks for existence of
+-- /etc/config/ubootenv file and does nothing.
+-- In case of Mox we also move fw_env.config from mox-support package. Because of
+-- that we have to update mox-support first so we would not remove generated file.
+if not version_match or not installed or
+		(installed["uboot-tools"] and version_match(installed["uboot-tools"].version, "<2018.03-4")) then
+	Package("uboot-tools", { deps = "fix-uboot-env-reset" })
+	if board ~= "mox" then
+		Package("uboot-tools", { deps = "mox-support" })
+	end
+end

commit d252114524562f2e01e31ee5a6f49aa7c8a001c1

lists/base-fix: fix invalid package name for U-boot env changes
Even in Turris-build repository, we have patch to move uboot-envtools to
uboot-tools folder (and changed PKG_NAME), the final naming of package was not changed and it produces uboot-envtools package

Fixes: b50ca55cdcf4bcd6482ebd7b0eedb4597c92bc3a ("lists/base-fix: Reset
u-boot environment configuration")

lists/base-fix.lua
@@ -137,16 +137,16 @@ if installed and installed["luci-lighttpd"] and not installed["turris-webapps-lu
 	Install("luci-base", { reinstall = true, condition = "luci-base" })
 end
 
--- With uboot-tools version 2018.03-4 environment configuration was fixed. Problem
+-- With uboot-envtools version 2018.03-4 environment configuration was fixed. Problem
 -- is that it is not applied in default as script checks for existence of
 -- /etc/config/ubootenv file and does nothing.
 -- In case of Mox we also move fw_env.config from mox-support package. Because of
 -- that we have to update mox-support first so we would not remove generated file.
 if not version_match or not installed or
-		(installed["uboot-tools"] and version_match(installed["uboot-tools"].version, "<2018.03-4")) then
-	Package("uboot-tools", { deps = "fix-uboot-env-reset" })
+		(installed["uboot-envtools"] and version_match(installed["uboot-envtools"].version, "<2018.03-4")) then
+	Package("uboot-envtools", { deps = "fix-uboot-env-reset" })
 	if board ~= "mox" then
-		Package("uboot-tools", { deps = "mox-support" })
+		Package("uboot-envtools", { deps = "mox-support" })
 	end
 end
 